6dea4aa90432cdc0c768fd8c6138c8b2e998a7fb
Kennn05 <bosslevelgod2@gmail.com>
2023-12-02 14:28:05 +0800

    Your commit message

--- upload.sh
+++ upload.sh
@@ -1,9 +1,19 @@
-git init 
+#!/bin/bash
 
-git add . 
+# Your GitHub username
+USERNAME=Kennn05
 
-git commit -m "new" 
+# Your GitHub token
+TOKEN=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
 
-git remote add origin https://Kennn05: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@github.com/Kennn05/the-eyes.git
+# Update the remote origin URL with the username and token
+git remote set-url origin https://${USERNAME}:${TOKEN}@github.com/your-username/your-repo.git
 
+# Add all files except upload.sh
+git add .
+
+# Commit changes
+git commit -m "Your commit message"
+
+# Push to GitHub
 git push -u origin master
