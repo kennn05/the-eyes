6e48f34e41d32c3f5f5a7dfda2c848dc67b310a5
Kennn05 <bosslevelgod2@gmail.com>
2023-12-02 14:26:46 +0800

    new

---
+++ upload.sh
@@ -0,0 +1,9 @@
+git init 
+
+git add . 
+
+git commit -m "new" 
+
+git remote add origin https://Kennn05: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@github.com/Kennn05/the-eyes.git
+
+git push -u origin master
